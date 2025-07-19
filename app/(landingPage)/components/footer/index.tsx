e48c30eaf53d980b52200b3d0370ac4f100eda25
tasammie <temiloluwasamuel4@gmail.com>
2025-07-19 20:46:01 +0100

    fresh page update

--- app/(landingPage)/components/footer/index.tsx
+++ app/(landingPage)/components/footer/index.tsx
@@ -57,7 +57,7 @@ const LighthouseFooter = () => {
                   href="#"
                   className="text-gray-300 hover:text-white transition-colors"
                 >
-                  Link Three
+                  Link Threeee
                 </a>
               </li>
               <li>
