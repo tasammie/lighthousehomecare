c3489f38beac62ff3aacbd1d634a4fa4da19011a
tasammie <temiloluwasamuel4@gmail.com>
2025-07-21 19:16:00 +0100

    more page

--- app/(landingPage)/components/footer/index.tsx
+++ app/(landingPage)/components/footer/index.tsx
@@ -57,7 +57,7 @@ const LighthouseFooter = () => {
                   href="#"
                   className="text-gray-300 hover:text-white transition-colors"
                 >
-                  Link Three
+                  Link Threegg
                 </a>
               </li>
               <li>
