bde828b4bd1a1bdf182fcbe5a592555539f917b5
oslim <adewaleselim6@gmail.com>
2026-07-23 13:26:57 +0100

    refactor: update environment variable handling in sanity configuration

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,13 +1,16 @@
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-10-01";
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION ||
+  process.env.SANITY_STUDIO_API_VERSION ||
+  "2023-10-01";
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+  process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET / SANITY_STUDIO_DATASET'
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_STUDIO_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID / SANITY_STUDIO_PROJECT_ID'
 )
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
